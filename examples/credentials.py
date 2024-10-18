69f26952a70a78080bec15a544eda7d7216e2817
Philip de Nier <philipn@rd.bbc.co.uk>
2024-10-18 14:13:04 +0100

    Add some example python code using the TAMS API

---
+++ examples/credentials.py
@@ -0,0 +1,76 @@
+# This file provides functions to handle TAMS API credentials
+
+from abc import ABCMeta, abstractmethod
+import base64
+
+import aiohttp
+
+
+def get_basic_auth_header(name: str, secret: str) -> dict[str, str]:
+    creds = base64.b64encode(f"{name}:{secret}".encode()).decode()
+    return {"Authorization": f"Basic {creds}"}
+
+
+class Credentials(metaclass=ABCMeta):
+    """Base class for TAMS API credentials"""
+    @abstractmethod
+    def header(self) -> dict[str, str]:
+        """Returns the Authorization HTTP header"""
+        return {}
+
+
+class BasicCredentials(Credentials):
+    """Basic username/password credentials"""
+    def __init__(self, username: str, password: str) -> None:
+        self._header = get_basic_auth_header(username, password)
+
+    def header(self) -> dict[str, str]:
+        return self._header
+
+
+class RenewableCredentials(Credentials):
+    """Base class for credentials that need to be renewed"""
+    @abstractmethod
+    async def ensure_credentials(self) -> None:
+        pass
+
+    @abstractmethod
+    async def renew_credentials(self) -> None:
+        pass
+
+
+class OAuth2ClientCredentials(RenewableCredentials):
+    """OAuth2 Client Credentials Grant credentials"""
+    def __init__(
+        self,
+        url: str,
+        client_id: str,
+        client_secret: str,
+    ) -> None:
+        self.url = url
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.access_token = ""
+        self.expires_in = 0.0
+
+    async def ensure_credentials(self) -> None:
+        if not self.access_token:
+            await self.renew_credentials()
+
+    async def renew_credentials(self) -> None:
+        form_data = {
+            "grant_type": "client_credentials",
+            "scope": "openid"
+        }
+        headers = get_basic_auth_header(self.client_id, self.client_secret)
+
+        async with aiohttp.ClientSession() as session:
+            async with session.post(self.url, data=form_data, headers=headers) as resp:
+                resp.raise_for_status()
+
+                token_response = await resp.json()
+                self.access_token = token_response["access_token"]
+                self.expires_in = token_response["expires_in"]
+
+    def header(self) -> dict[str, str]:
+        return {"Authorization": f"Bearer {self.access_token}"}
